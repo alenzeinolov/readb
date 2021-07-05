6af8f9967fc0502ceb694eee1877274ecc26ed38
Alen Zeinolov <github@alenzeinolov.com>
2021-07-05 22:51:09 +0600

    Add basic server setup

---
+++ config/asgi.py
@@ -0,0 +1,21 @@
+"""
+ASGI config for readb project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+import sys
+from pathlib import Path
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "readb.settings")
+
+ROOT_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(str(ROOT_DIR / "apps"))
+
+application = get_asgi_application()
---
+++ config/settings/base.py
@@ -0,0 +1,134 @@
+from pathlib import Path
+
+import environ
+
+# Dirs
+ROOT_DIR = Path(__file__).resolve().parent.parent.parent
+APPS_DIR = ROOT_DIR / "apps"
+
+# Envs
+env = environ.Env()
+env.read_env(str(ROOT_DIR / ".env"))
+
+# Secrets
+SECRET_KEY = env.str("SECRET_KEY")
+
+# Debug
+DEBUG = env.bool("DEBUG", default=False)
+
+# Allowed hosts
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
+
+# Apps
+DJANGO_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
+
+THIRD_PARTY_APPS = []
+
+LOCAL_APPS = [
+    "accounts",
+]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+
+# Middleware
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+# URLs
+ROOT_URLCONF = "config.urls"
+
+# Templates
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+# WSGI
+WSGI_APPLICATION = "config.wsgi.application"
+
+# Database
+DATABASES = {
+    "default": env.db("DATABASE_URL"),
+}
+
+# Cache
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": env.str("REDIS_URL"),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
+
+# Session
+SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
+
+# Cookies
+CSRF_COOKIE_NAME = "readb-csrftoken"
+SESSION_COOKIE_NAME = "readb-sessionid"
+
+# Auth
+AUTH_USER_MODEL = "accounts.User"
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+]
+
+# Intl
+LANGUAGE_CODE = "en"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+# Static
+STATIC_URL = "/static/"
+
+# Default AutoField
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
