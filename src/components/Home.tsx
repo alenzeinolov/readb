7b592f2412a2afef7e4c4c8c29a0b5c36c4eb40f
Alen Zeinolov <github@alenzeinolov.com>
2021-07-20 22:39:31 +0600

    Add home page fetching

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,32 @@
 import useAuth from "../hooks/useAuth";
+import { useQuery } from "react-query";
+import axios from "axios";
 
 const Home = () => {
   const auth = useAuth();
 
+  const { data, isLoading, isError } = useQuery("books", async () => {
+    const res = await axios.get(
+      process.env.REACT_APP_SERVER_URL + "/api/v1/books",
+      {
+        headers: {
+          Authorization: `Token ${auth?.accessToken}`,
+        },
+      }
+    );
+    console.log(res.data);
+    return res.data;
+  });
+
+  if (isError) return <div>Error fetching books</div>;
+
+  if (isLoading) return <div>Loading...</div>;
+
   return (
     <div>
-      {auth?.accessToken ? "token: " + auth.accessToken : "unauthorized"}
-      This is home page.
+      {data.map((book: any) => (
+        <div>book.name</div>
+      ))}
     </div>
   );
 };
