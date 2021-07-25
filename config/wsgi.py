96feb735a7183ca29cdd73a31121a9d903c41931
Alen Zeinolov <github@alenzeinolov.com>
2021-07-25 12:14:54 +0600

    Add initial backend setup

---
+++ config/wsgi.py
@@ -0,0 +1,21 @@
+"""
+WSGI config for readb project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+import sys
+from pathlib import Path
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+ROOT_DIR = Path(__file__).resolve().parent.parent
+sys.path.append(str(ROOT_DIR / "apps"))
+
+application = get_wsgi_application()
