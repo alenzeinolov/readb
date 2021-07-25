24f3a4e20e3b27ccff40f41967485c0bd1e4582b
Alen Zeinolov <github@alenzeinolov.com>
2021-07-25 17:32:18 +0600

    Add Author and Book models

---
+++ apps/books/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from books.models import Author, Book
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("full_name",)
+    search_fields = ("full_name",)
+    fields = ("full_name",)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("title", "isbn", "published_on")
+    search_fields = ("title", "isbn")
+    fields = ("title", "authors", "isbn", "published_on")
+    filter_horizontal = ("authors",)
---
+++ apps/books/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class Author(models.Model):
+    full_name = models.CharField(max_length=500)
+
+    class Meta:
+        ordering = ["full_name"]
+
+    def __str__(self):
+        return self.full_name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=500)
+    isbn = models.CharField(unique=True, max_length=50)
+    authors = models.ManyToManyField(
+        Author,
+        related_name="books",
+        related_query_name="book",
+    )
+    published_on = models.DateTimeField()
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
