adcbdaaf5b968b7706071bbbefc1766e711ae677
Alen Zeinolov <github@alenzeinolov.com>
2021-07-25 16:37:38 +0600

    Add DEBUG as env variable

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ env.read_env(str(ROOT_DIR / ".env"))
 SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG")
 
 ALLOWED_HOSTS = ["*"]
 
