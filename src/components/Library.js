33827510fc6c153ce69a8336e8041f6ac176c61c
Alen Zeinolov <github@alenzeinolov.com>
2021-09-05 23:01:45 +0600

    Wire up the rest of the functionality and add filters to library

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,9 +1,17 @@
-import { Skeleton, Spinner, Stack, Text, VStack } from "@chakra-ui/react";
+import {
+  Button,
+  ButtonGroup,
+  Flex,
+  Spinner,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { useQuery } from "react-query";
 import { USER_BOOKS_URL } from "../constants";
 import { useAuth } from "./AuthProvider";
 import axios from "axios";
 import LibraryBook from "./LibraryBook";
+import { useState } from "react";
 
 function Library() {
   const auth = useAuth();
@@ -17,17 +25,56 @@ function Library() {
     return res.data;
   });
 
+  const [filterStatus, setFilterStatus] = useState("");
+
+  const toggleFilterStatus = (status) => {
+    if (filterStatus === status) {
+      setFilterStatus("");
+    } else {
+      setFilterStatus(status);
+    }
+  };
+
   return (
-    <VStack mt={6}>
-      {isLoading ? <Spinner /> : null}
-      {data ? (
-        data.map((userBook) => (
-          <LibraryBook key={userBook.id} userBook={userBook} />
-        ))
-      ) : (
-        <Text>There are no books in the library yet...</Text>
-      )}
-    </VStack>
+    <>
+      <Flex mt={6} justifyContent="center">
+        <ButtonGroup isAttached>
+          <Button
+            isActive={filterStatus === "WANT_TO_READ"}
+            onClick={() => toggleFilterStatus("WANT_TO_READ")}
+          >
+            Want to read
+          </Button>
+          <Button
+            isActive={filterStatus === "READING"}
+            onClick={() => toggleFilterStatus("READING")}
+          >
+            Reading
+          </Button>
+          <Button
+            onClick={() => toggleFilterStatus("READ")}
+            isActive={filterStatus === "READ"}
+          >
+            Read
+          </Button>
+        </ButtonGroup>
+      </Flex>
+      <VStack mt={6}>
+        {isLoading ? <Spinner /> : null}
+        {data && data.length > 0 ? (
+          data
+            .filter((userBook) => {
+              if (!filterStatus) return true;
+              return userBook.status === filterStatus;
+            })
+            .map((userBook) => (
+              <LibraryBook key={userBook.id} userBook={userBook} />
+            ))
+        ) : (
+          <Text>There are no books in the library yet...</Text>
+        )}
+      </VStack>
+    </>
   );
 }
 
--- src/components/LibraryBook.js
+++ src/components/LibraryBook.js
@@ -5,19 +5,82 @@ import {
   Heading,
   Menu,
   MenuButton,
+  MenuDivider,
   MenuItem,
   MenuList,
   Text,
+  useToast,
 } from "@chakra-ui/react";
 import { FiChevronDown } from "react-icons/fi";
+import { useMutation, useQueryClient } from "react-query";
+import axios from "axios";
+import { USER_BOOKS_URL } from "../constants";
+import { useAuth } from "./AuthProvider";
+import { useState } from "react";
 
 const STATUS_COLORS = {
   WANT_TO_READ: "blue",
   READING: "purple",
-  READ: "success",
+  READ: "green",
 };
 
 function LibraryBook({ userBook }) {
+  const auth = useAuth();
+  const toast = useToast();
+  const client = useQueryClient();
+
+  const changeStatusMutation = useMutation(async ({ userBookId, status }) => {
+    try {
+      await axios.patch(
+        `${USER_BOOKS_URL}${userBookId}/`,
+        {
+          status,
+        },
+        {
+          headers: {
+            Authorization: `Token ${auth.data.token}`,
+          },
+        }
+      );
+      toast({
+        description: "Book status has been updated successfully.",
+        status: "success",
+      });
+      await client.invalidateQueries("user-books");
+    } catch (err) {
+      toast({
+        description: "There was an error editing the book status.",
+        status: "error",
+      });
+    }
+  });
+  const deleteMutation = useMutation(async (userBookId) => {
+    try {
+      await axios.delete(`${USER_BOOKS_URL}${userBookId}/`, {
+        headers: {
+          Authorization: `Token ${auth.data.token}`,
+        },
+      });
+      toast({
+        description: "Book has been deleted successfully.",
+        status: "success",
+      });
+      await client.invalidateQueries("user-books");
+    } catch (err) {
+      toast({
+        description: "There was an error deleting the book.",
+        status: "error",
+      });
+    }
+  });
+
+  const handleStatusChange = (statusIdx) => {
+    changeStatusMutation.mutate({
+      userBookId: userBook.id,
+      status: Object.keys(STATUS_COLORS)[statusIdx],
+    });
+  };
+
   return (
     <Flex
       p={4}
@@ -52,10 +115,18 @@ function LibraryBook({ userBook }) {
           Actions
         </MenuButton>
         <MenuList>
-          <MenuItem>Want to read</MenuItem>
-          <MenuItem>Reading</MenuItem>
-          <MenuItem>Read</MenuItem>
-          <MenuItem>Delete</MenuItem>
+          <MenuItem onClick={() => handleStatusChange(0)}>
+            Want to read
+          </MenuItem>
+          <MenuItem onClick={() => handleStatusChange(1)}>Reading</MenuItem>
+          <MenuItem onClick={() => handleStatusChange(2)}>Read</MenuItem>
+          <MenuDivider />
+          <MenuItem
+            color="red"
+            onClick={() => deleteMutation.mutate(userBook.id)}
+          >
+            Delete
+          </MenuItem>
         </MenuList>
       </Menu>
     </Flex>
